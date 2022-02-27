ce4e48d6bb4ed126d94378971b7b35693ef18a0d
starkvr1 <prateekpatriotasme@gmail.com>
2022-02-28 01:50:30 +0530

    second commit

--- courserecommender/settings.py
+++ courserecommender/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vivy&1q3*ji$1dc50x4wq@v@187=5hc2u1!6%u$mm*x_0$&fp9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [https://urcourse.herokuapp.com/]
+ALLOWED_HOSTS = ['urcourse.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
